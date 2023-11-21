ea224fcb4ee3d87cccd793a1296b74fb76e08b8c
Jörg Richter <jri@deepamehta.de>
2023-11-21 17:22:13 +0100

    Setup plugin project

---
+++ src/main/java/systems/dmx/tesseract/TesseractPlugin.java
@@ -0,0 +1,20 @@
+package systems.dmx.tesseract;
+
+import systems.dmx.core.osgi.PluginActivator;
+
+import java.util.logging.Logger;
+
+
+
+public class TesseractPlugin extends PluginActivator {
+
+    // ------------------------------------------------------------------------------------------------------- Constants
+
+    // ---------------------------------------------------------------------------------------------- Instance Variables
+
+    private Logger logger = Logger.getLogger(getClass().getName());
+
+    // -------------------------------------------------------------------------------------------------- Public Methods
+
+    // ------------------------------------------------------------------------------------------------- Private Methods
+}
