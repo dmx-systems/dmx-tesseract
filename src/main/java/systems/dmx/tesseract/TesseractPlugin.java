bf36d3b597349ce1c7d0153a4344d8e2c5cfcaff
Jörg Richter <jri@deepamehta.de>
2023-11-22 12:41:14 +0100

    Setup Tess4J library

--- src/main/java/systems/dmx/tesseract/TesseractPlugin.java
+++ src/main/java/systems/dmx/tesseract/TesseractPlugin.java
@@ -1,7 +1,13 @@
 package systems.dmx.tesseract;
 
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+// import net.sourceforge.tess4j.Tesseract1;
+
 import systems.dmx.core.osgi.PluginActivator;
 
+import javax.imageio.ImageIO;
+import java.io.File;
 import java.util.logging.Logger;
 
 
@@ -15,6 +21,28 @@ public class TesseractPlugin extends PluginActivator {
     private Logger logger = Logger.getLogger(getClass().getName());
 
     // -------------------------------------------------------------------------------------------------- Public Methods
+
+    // *** Hooks ***
+
+    @Override
+    public void init() {
+        try {
+            // ImageIO.scanForPlugins();                    // for server environment (?)
+            // File dataDir = new File("/Users/jri/Downloads/Tess4j/tessdata");
+            File dataDir = new File("/usr/local/Cellar/tesseract/5.2.0/share/tessdata");      // TODO: make configurable
+            File imageFile = new File("test-data/eurotext.png");
+            logger.info("### Data dir " + dataDir + ", exists=" + dataDir.exists());
+            logger.info("### Reading image file " + imageFile.getAbsolutePath() + ", exists=" + imageFile.exists());
+            ITesseract instance = new Tesseract();          // JNA Interface Mapping
+            // ITesseract instance = new Tesseract1();      // JNA Direct Mapping (?)
+            instance.setDatapath(dataDir.getPath());
+            // instance.setLanguage("eng");
+            String result = instance.doOCR(imageFile);
+            System.out.println(result);
+        } catch (Exception e) {
+            throw new RuntimeException("OCR failed", e);
+        }
+    }
 
     // ------------------------------------------------------------------------------------------------- Private Methods
 }
