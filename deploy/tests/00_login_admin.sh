d0cd49c48a2f5ae16a7ca93ff41735ea01c04ec3
Juergen Neumann <juergen@junes.eu>
2024-03-03 13:06:03 +0100

    CI: improve admin login

--- deploy/tests/00_login_admin.sh
+++ deploy/tests/00_login_admin.sh
@@ -11,6 +11,7 @@ sleep 3
 ## requires vars should be pre-set via environment
 USERNAME='admin'
 PASSWORD="${DMX_ADMIN_PASSWORD}"
+
 ## if env is empty, set default
 if [ -z "${TIER}" ]; then
     export TIER='dev'
@@ -20,14 +21,16 @@ if [ -z "${WEB_URL}" ] && [ "${CI_COMMIT_BRANCH}" == "master" -o "${CI_COMMIT_BR
 elif [ -z "${WEB_URL}" ] && [ "${CI_COMMIT_BRANCH}" != "master" -a "${CI_COMMIT_BRANCH}" == "main" ]; then
     WEB_URL="${CI_COMMIT_REF_SLUG}_${CI_PROJECT_NAME}-${TIER}.ci.dmx.systems"
 fi
-## export HOST for subsequent scripts
+
+## export HOST for subsequent test scripts
 export HOST="https://${WEB_URL}:443"
+
 ## Test access to Administration workspace to ensure login as admin was successful.
 URL='core/topic/uri/dmx.workspaces.administration'
 BASE64="$( echo -n "${USERNAME}:${PASSWORD}" | base64 )"
 AUTH="Authorization: Basic ${BASE64}"
 SESSION="$( curl -sS -H "${AUTH}" "${HOST}/${URL}" -i 2>&1 )"
-HTTPCODE="$( echo "${SESSION}" | grep HTTP | cut -d' ' -f2 )"
+HTTPCODE="$( echo "${SESSION}" | grep HTTP | cut -d' ' -f2 | sed 's/[^0-9]//g' )"
 if [ "${HTTPCODE}" != "200" -a "${HTTPCODE}" != "204" ]; then
     echo "ERROR! Login ${USERNAME} at ${HOST} failed! (HTTPCODE=${HTTPCODE})"
     exit 1
